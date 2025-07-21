2f59cd9ce97ba844d33bc618252398ceef573c8f
Foster Ametepey <122822016+Greattero@users.noreply.github.com>
2025-07-21 16:29:30 +0000

    Fix db availability checker

--- server/server.js
+++ server/server.js
@@ -85,8 +85,8 @@ app.post("/api/checkin-details", async (req, res) => {
 
     // Check if collection exists first
     const collections = await mongoose.connection.db.listCollections({ name: programme }).toArray();
-    if (collections.length > 0) {
-      return res.json({ dbAvailable: true });
+    if (collections.length === 0) {
+      return res.json({ dbAvailable: false });
     }
 
     // Now safely define the model
