ed829ac1ebef72f90ff9e374257d7f4a88fa8984
Foster Ametepey <122822016+Greattero@users.noreply.github.com>
2025-07-22 18:06:19 +0000

    Fix backend to check ip address

--- server/server.js
+++ server/server.js
@@ -98,7 +98,7 @@ app.post("/api/checkin-details", async (req, res) => {
     const Student = mongoose.model("Programme", studentSchema, programme);
 
     // Check if student already exists
-    const user = await Student.findOne({ index_no});
+    const user = await Student.findOne({ $or: [{ myip }, { index_no }] });
 
     if (user) {
       return res.json({ available: true });
