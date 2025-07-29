8edb7adb63c747ac917fcfd08906cdc915377eb9
Foster Ametepey <122822016+Greattero@users.noreply.github.com>
2025-07-29 14:09:48 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { ObjectId } from 'mongodb';
 
 const app = express();
 dotenv.config();
