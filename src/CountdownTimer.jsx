f09b303d4aae3ddc6eb7927b84bdc3cabe154906
Foster Ametepey <122822016+Greattero@users.noreply.github.com>
2025-08-07 06:57:11 +0000

    Update CountdownTimer.jsx

--- src/CountdownTimer.jsx
+++ src/CountdownTimer.jsx
@@ -16,7 +16,7 @@ const CountdownTimer = ({ hostTime, setHostTime, lockCheckin, unLockCheckin,prog
 
   const getAllNames = async () =>{
     try{
-      const response = await fetch(https://attendict.onrender.com/api/student-list?programme=${programme});
+      const response = await fetch("https://attendict.onrender.com/api/student-list?programme=${programme}");
       console.log(programme);
       const students = await response.json();
       return students;
@@ -145,3 +145,4 @@ const CountdownTimer = ({ hostTime, setHostTime, lockCheckin, unLockCheckin,prog
 };
 
 export default CountdownTimer;
+
