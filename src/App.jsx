1e9960614fc361a73e760560595507b15c2ea8da
Foster Ametepey <122822016+Greattero@users.noreply.github.com>
2026-01-06 10:03:25 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,7 @@ function App() {
 
       if (!Array.isArray(parsed) || parsed.length === 0) return;
 
-      const FIVE_MIN = 50000;
+      const FIVE_MIN = 5 * 60 * 1000; // 300,000 ms
 
       for (const item of parsed) {
         const [programme, time] = item.split("|");
