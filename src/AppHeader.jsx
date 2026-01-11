c84079b581764e97791de49f96ca27a24155abca
Foster Ametepey <122822016+Greattero@users.noreply.github.com>
2026-01-11 16:41:27 +0000

    Update AppHeader.jsx

--- src/AppHeader.jsx
+++ src/AppHeader.jsx
@@ -24,7 +24,7 @@ const Heading = styled.div`
 
   button {
     background-color: transparent;
-    color: #5f5f5f;
+    color: #c30707;
     border: none;
     cursor: pointer;
     display: flex;
@@ -80,6 +80,7 @@ function AppHeader({onLogout,disableLogout}){
 
 
 export default AppHeader;
+
 
 
 
