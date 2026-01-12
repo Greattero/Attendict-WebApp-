29ce7f157e416405d795c06cd68154c3b858fd61
Foster Ametepey <122822016+Greattero@users.noreply.github.com>
2026-01-12 14:40:52 +0000

    Update AppHeader.jsx

--- src/AppHeader.jsx
+++ src/AppHeader.jsx
@@ -6,7 +6,7 @@ const Heading = styled.div`
   background-color: rgb(241, 239, 239);;
   margin-top: -25px;
   height: 80px;
-  width:100vw;
+  width:100%;
   display: flex;
   align-items: center;
   justify-content: space-between;
