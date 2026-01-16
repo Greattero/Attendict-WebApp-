64c1ca89b61efac66d94e9efcc831654def57b9b
Foster Ametepey <122822016+Greattero@users.noreply.github.com>
2026-01-16 20:52:38 +0000

    Update LocationCoords.jsx

--- src/LocationCoords.jsx
+++ src/LocationCoords.jsx
@@ -4,7 +4,7 @@ import 'boxicons/css/boxicons.min.css';
 
 
 const LocationWrapper = styled.div`
-  margin-top: 95px;
+  margin-top: 75px;
   display: flex;
   align-items: center;
   justify-content: center;
