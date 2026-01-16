9ccf61d5664d66164eaaaa9be8dfbfeaf89aff73
Foster Ametepey <122822016+Greattero@users.noreply.github.com>
2026-01-16 16:40:15 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -199,13 +199,12 @@ function App() {
     )}
       
       <div style={{ // New wrapper div
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  justifyContent: "center",
-  height: "100vh",
-  gap: "1rem",
-  paddingTop: "20px"
+          flex: 1, 
+          display: "flex", 
+          justifyContent: "center", 
+          flexDirection: "column", 
+          alignItems: "center",   
+          paddingBottom: "250px",
       }}>
         <LocationCoords/>
         <Home onButtonClick={(type)=>handleButtonClick(type)} disabled={disable}/> {/* Remove all styles from Home */}
