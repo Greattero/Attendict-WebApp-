b59676fa9d707c3247da0b28fadecffa12769c18
Foster Ametepey <122822016+Greattero@users.noreply.github.com>
2026-01-16 16:50:12 +0000

    Update LocationCoords.jsx

--- src/LocationCoords.jsx
+++ src/LocationCoords.jsx
@@ -11,8 +11,8 @@ const LocationWrapper = styled.div`
     gap: 2rem;
     position: absolute;
     top: 5;
-    margin-bottom: 3rem; /* NOT huge */
-    margin-top: 10px;
+    // margin-bottom: 3rem; /* NOT huge */
+    // margin-top: 10px;
     background-color: red;
   }
 
