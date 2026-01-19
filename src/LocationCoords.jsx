eb4d58665c6437ec984f0f994e3b2809374ad199
Foster Ametepey <122822016+Greattero@users.noreply.github.com>
2026-01-19 12:55:09 +0000

    Update LocationCoords.jsx

--- src/LocationCoords.jsx
+++ src/LocationCoords.jsx
@@ -39,8 +39,8 @@ export default function LocationCoords({locationValues}){
       },
       (err) => {
       if (err.code === 1) alert("Permission denied.Turn on your phone’s location or reset browser permissions to allow access, then refresh page.");
-      if (err.code === 2) alert("Position unavailable");
-      if (err.code === 3) alert("Timeout");
+      if (err.code === 2) alert("Position unavailable. Refresh page and try again");
+      if (err.code === 3) alert("Timeout. Refresh page and try again");
     }
     );
 }, []);
