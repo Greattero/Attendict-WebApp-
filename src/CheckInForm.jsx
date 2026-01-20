ab257e3b498f938bd370c492fdcefcdb2c60362c
Foster Ametepey <122822016+Greattero@users.noreply.github.com>
2026-01-20 20:25:35 +0000

    Update CheckInForm.jsx

--- src/CheckInForm.jsx
+++ src/CheckInForm.jsx
@@ -308,7 +308,7 @@ useEffect(()=>{
     const t = setTimeout(()=>{
       setPendingSubmit(false);
       submitData();
-    }, 10000)
+    }, 50000)
 
     return () => clearTimeout(t); // 👈 REQUIRED
 
@@ -481,6 +481,7 @@ const submitData = async()=>{
 }
 
 export default CheckInForm;
+
 
 
 
