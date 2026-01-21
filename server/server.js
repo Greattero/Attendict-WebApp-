8b555d84e2e0fc40c2e73b964e3bf59bde037309
Foster Ametepey <122822016+Greattero@users.noreply.github.com>
2026-01-21 07:52:17 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -223,7 +223,7 @@ app.post("/api/checkin-details", async (req, res) => {
     }
 
     let inspect = "0";
-    if (distance > 0.4 && distance <= 0.9) inspect = "1";
+    if (distance > 0.5 && distance <= 0.9) inspect = "1";
 
 
     // Save the new student
@@ -494,6 +494,7 @@ app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 
 });
+
 
 
 
