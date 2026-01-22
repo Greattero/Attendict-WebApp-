8b7109a13434b34a666e00b44fdbc658c577ec61
Foster Ametepey <122822016+Greattero@users.noreply.github.com>
2026-01-22 17:47:02 +0000

    Update HostForm.jsx

--- src/HostForm.jsx
+++ src/HostForm.jsx
@@ -374,7 +374,7 @@ function HostForm({onClose, setHostTime, setProgramme, sendFeedback, sendVisible
         display: "flex",
         flexDirection: "row",
         justifyContent: "space-between",
-        gap: 15,
+        gap: 25,
       }}>
         <Header>
                 HOST 
@@ -434,6 +434,7 @@ function HostForm({onClose, setHostTime, setProgramme, sendFeedback, sendVisible
 
 
 export default HostForm;
+
 
 
 
