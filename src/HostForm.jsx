8f335c2360b8bf0f1435de87debbe2eed3be6865
Foster Ametepey <122822016+Greattero@users.noreply.github.com>
2026-01-22 18:03:27 +0000

    Update HostForm.jsx

--- src/HostForm.jsx
+++ src/HostForm.jsx
@@ -377,7 +377,7 @@ function HostForm({onClose, setHostTime, setProgramme, sendFeedback, sendVisible
     <Hosting ref= {popupRef}>
         <Header>
             HOST
-            <i className='bx bx-x-circle' style={{ color: '#628245', marginTop: "4px" }}/>
+            <i className='bx bx-x-circle' style={{ color: '#628245', marginTop: "3px" }}/>
         </Header>
 
         <LabelHint>(for Lecturers/class reps only)</LabelHint>
@@ -433,6 +433,7 @@ function HostForm({onClose, setHostTime, setProgramme, sendFeedback, sendVisible
 
 
 export default HostForm;
+
 
 
 
