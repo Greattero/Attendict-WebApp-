71e2f94a977c2b8054d60e7898e41cd6321d7dd3
Foster Ametepey <122822016+Greattero@users.noreply.github.com>
2026-01-29 16:08:02 +0000

    Update Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -328,7 +328,7 @@ function Login({onLoginSuccess, sendFeedback, sendVisible}){
                             display: "flex",
                             flexDirection: "row",
                             gap: "2px",
-                            justifyContent: "center";
+                            justifyContent: "center",
                             
                         }}>
                             <button>
@@ -371,6 +371,7 @@ function Login({onLoginSuccess, sendFeedback, sendVisible}){
 }
 
 export default Login;
+
 
 
 
