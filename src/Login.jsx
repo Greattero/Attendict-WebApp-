1369609c8ee960d46d0dad618def5139798b00b4
Foster Ametepey <122822016+Greattero@users.noreply.github.com>
2026-01-29 15:38:48 +0000

    Update Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -43,9 +43,9 @@ const FormBox = styled.form`
     width: 100%;
     height: 65%;
     //padding-top:45px;
-    margin-top: 300px;
+    margin-top: 220px;
     justify-content: flex-start;
-    background-color: red;
+    //background-color: red;
   }
 `
 const Form = styled.div`
@@ -349,6 +349,7 @@ function Login({onLoginSuccess, sendFeedback, sendVisible}){
 }
 
 export default Login;
+
 
 
 
