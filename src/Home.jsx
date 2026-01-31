fcf516f562ff529a5e5d74acf5d7e63e6c5a8e70
Foster Ametepey <122822016+Greattero@users.noreply.github.com>
2026-01-31 10:47:26 +0000

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -205,7 +205,7 @@ const Notice = styled.label`
   padding: 10px;
 
   @media screen and (max-width: 650px) {
-    margin-top: -100px;
+    margin-top: -70px;
   }
 `;
 
@@ -253,6 +253,7 @@ function Home({onButtonClick, disabled, getWhoIAm}){
 
 
 export default Home;
+
 
 
 
