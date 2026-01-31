d4ed00fa56efbe2189047effb0d73187d3a2d4f2
Foster Ametepey <122822016+Greattero@users.noreply.github.com>
2026-01-31 10:41:47 +0000

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -14,7 +14,7 @@ const HomePage = styled.div`
     background-color: #dbdbdb;
     // padding-top: 1rem;     /* safe spacing */
     width: 100%;
-    margin-top: 10px;
+    margin-top: 60px;
   }
 
 `;
@@ -253,6 +253,7 @@ function Home({onButtonClick, disabled, getWhoIAm}){
 
 
 export default Home;
+
 
 
 
