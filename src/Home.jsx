8bdbb15588bd54aceaa8d7edee8ca94987978576
Foster Ametepey <122822016+Greattero@users.noreply.github.com>
2026-01-31 09:31:08 +0000

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -28,6 +28,7 @@ const Buttons = styled.div`
   @media screen and (max-width: 650px) {
     flex-direction: column;
     align-items: center;
+    justify-content: center;
     gap: 2rem;
     min-height: 60vh; /* 👈 KEY */
     margin-top: -25px;
@@ -250,6 +251,7 @@ function Home({onButtonClick, disabled, getWhoIAm}){
 
 
 export default Home;
+
 
 
 
