f2f90fa91e9eb2fad9ee7a7779fa47fc57e4fecc
Foster Ametepey <122822016+Greattero@users.noreply.github.com>
2026-02-03 14:06:43 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -223,7 +223,7 @@ app.post("/api/checkin-details", async (req, res) => {
     }
 
     let inspect = "0";
-    if (distance > 0.045 && distance <= 0.100) inspect = "1";
+    if (distance > 0.045 && distance <= 0.120) inspect = "1";
 
 
     // Save the new student
