55826e4c8048118e549c49ce00f9e7b76bfa060e
Foster Ametepey <122822016+Greattero@users.noreply.github.com>
2026-03-16 23:43:20 +0000

    Refactor AppHeader component with hooks and styles

--- src/AppHeader.jsx
+++ src/AppHeader.jsx
@@ -1,4 +1,4 @@
-import React, { useState} from "react";
+import React, { useState, useEffect } from "react";
 import styled, { keyframes } from "styled-components";
 import CheckInForm from "./CheckInForm.jsx";
 import CountdownTimer from "./CountdownTimer.jsx";
