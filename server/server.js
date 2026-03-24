1fadf308bd4a8fe023abce1d36d37b22e1a7e21d
Foster Ametepey <122822016+Greattero@users.noreply.github.com>
2026-03-24 16:03:30 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -495,9 +495,9 @@ app.post("/api/login-details", async (req, res) => {
     }
 else if (
     (existingSession &&
-    new Date(existingSession.expiryTime) < new Date() &&
-    existingSession.username !== username) || 
-    existingSession.checkedIn === false
+    new Date(existingSession?.expiryTime) < new Date() &&
+    existingSession?.username !== username) || 
+    existingSession?.checkedIn === false
 )
  {
   // ✅ Expired + different user → deactivate old session
